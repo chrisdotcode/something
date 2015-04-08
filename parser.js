b48318b6c5b691fc01d820e1812c70229272034a
Chris Blake <chris@code.sc>
2015-04-08 17:01:17 -0400

    Enhances parser.
    - Renames 'item' to 'task'.
    - Fixes bug that caused list of tags to not populate.
    - Adds `toString` functions for tasks.
    - Exports all parsing/`toString` functions.

--- parser.js
+++ parser.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var TAG_REGEX = /(#\S+)\s*/g;
-var ITEM_REGEX = /^\s*>\s*(.*)/;
-// COMPLETED_REGEX = Leading strikethroughs + ITEM_REGEX
+var TASK_REGEX = /^\s*>\s*(.*)/;
+// COMPLETED_REGEX = Leading strikethroughs + TASK_REGEX
 var COMPLETED_REGEX = /^\s*--\s*>\s*(.*)/;
 
 function parseInput(input) {
@@ -11,16 +11,12 @@ function parseInput(input) {
     .filter(function(x) { return x != null; });
 
   var tags = tasks.reduce(function(_tags, task) {
-    if (task.tags.length > 0) {
-      _tags.concat(task.tags);
-    }
-
-    return _tags;
+    return task.tags.length > 0 ? _tags.concat(task.tags) : _tags;
   }, []);
 
   return {
     tasks: tasks,
-    tags: tags
+    tags: tags,
   };
 }
 
@@ -35,15 +31,14 @@ function parseLine(line) {
   var match = null;
 
   if (match = line.match(COMPLETED_REGEX)) {
-    completed= true;
-  } else if (match = line.match(ITEM_REGEX)) {
-    // Match on regular item; no pre-processing necessary.
+    completed = true;
+  } else if (match = line.match(TASK_REGEX)) {
+    // Match on regular task; no pre-processing necessary.
   } else {
     return null;
   }
 
-  // match[1] refers to the item without leading '>' (and extraneous
-  // whitespace, if any).
+  // match[1] refers to just the task contents.
   var task = match[1].replace(TAG_REGEX, function(_, tag) {
       tags.push(tag);
       return '';
@@ -56,4 +51,17 @@ function parseLine(line) {
   };
 }
 
-module.exports = parseInput;
+function taskToString(task) {
+  return (task.completed ? '-- ' : '') + '> ' + task.task + task.tags.join(' ');
+}
+
+function tasksToString(tasks) {
+  return tasks.map(taskToString).join('\n');
+}
+
+module.exports = exports = {
+  parseInput: parseInput,
+  parseLine: parseLine,
+  taskToString: taskToString,
+  tasksToString: tasksToString,
+};
